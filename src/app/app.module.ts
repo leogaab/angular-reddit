e7a61a39902173426e56d557f80ba8cec64afb3d
Leo Gaab <leonelgaab@gmail.com>
2020-03-13 23:29:46 -0300

    Form created to submit a title and a link for the article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { AddLinkFormComponent } from './components/add-link-form/add-link-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddLinkFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/add-link-form/add-link-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-link-form',
+  templateUrl: './add-link-form.component.html',
+  styleUrls: ['./add-link-form.component.css']
+})
+export class AddLinkFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
